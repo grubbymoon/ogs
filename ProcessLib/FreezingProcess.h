055df415e614f188e73fb834a035663af58c01a5
Zheng <ichbinzty@gmail.com>
2016-04-15 15:40:18 +0200

    add freezing process

---
+++ ProcessLib/FreezingProcess.h
@@ -0,0 +1,188 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#ifndef PROCESS_LIB_FREEZINGPROCESS_H_
+#define PROCESS_LIB_FREEZINGPROCESS_H_
+
+#include <cassert>
+
+#include <boost/optional.hpp>
+
+#include "AssemblerLib/LocalAssemblerBuilder.h"
+#include "AssemblerLib/LocalDataInitializer.h"
+
+#include "FreezingFEM.h"
+#include "Process.h"
+
+namespace MeshLib
+{
+    class Element;
+    class Mesh;
+    template <typename PROP_VAL_TYPE> class PropertyVector;
+}
+
+namespace ProcessLib
+{
+
+template<typename GlobalSetup>
+class FreezingProcess final
+        : public Process<GlobalSetup>
+{
+    // TODO change "this->" to "Base::"
+    using Base = Process<GlobalSetup>;
+
+public:
+    using GlobalMatrix = typename GlobalSetup::MatrixType;
+    using GlobalVector = typename GlobalSetup::VectorType;
+
+
+    FreezingProcess(
+        MeshLib::Mesh& mesh,
+        typename Process<GlobalSetup>::NonlinearSolver& nonlinear_solver,
+        std::unique_ptr<typename Process<GlobalSetup>::TimeDiscretization>&& time_discretization,
+        ProcessVariable& variable,
+        Parameter<double, MeshLib::Element const&> const&
+            thermal_conductivity)
+        : Process<GlobalSetup>(mesh, nonlinear_solver, std::move(time_discretization)),
+          _thermal_conductivity(thermal_conductivity)
+    {
+        this->_process_variables.emplace_back(variable);
+
+        if (dynamic_cast<NumLib::ForwardEuler<GlobalVector>*>(
+                    &Base::getTimeDiscretization()) != nullptr)
+        {
+            ERR("FreezingProcess can not be solved with the ForwardEuler"
+                " time discretization scheme. Aborting");
+            // Because the M matrix is not assembled. Thus, the linearized system
+            // would be singular. The same applies to CrankNicolson with theta = 0.0,
+            // but this case is not checked here.
+            // Anyway, the FreezingProcess shall be transferred to a simpler
+            // ODESystemTag in the future.
+            std::abort();
+        }
+    }
+
+    template <unsigned GlobalDim>
+    void createLocalAssemblers()
+    {
+        DBUG("Create local assemblers.");
+        // Populate the vector of local assemblers.
+        _local_assemblers.resize(this->_mesh.getNElements());
+        // Shape matrices initializer
+        using LocalDataInitializer = AssemblerLib::LocalDataInitializer<
+            Freezing::LocalAssemblerDataInterface,
+            Freezing::LocalAssemblerData,
+            typename GlobalSetup::MatrixType,
+            typename GlobalSetup::VectorType,
+            GlobalDim,
+            Parameter<double, MeshLib::Element const&> const&
+            /*Parameter...*/>;   // copy everytime when add parameters
+
+        LocalDataInitializer initializer;
+
+        using LocalAssemblerBuilder =
+            AssemblerLib::LocalAssemblerBuilder<
+                MeshLib::Element,
+                LocalDataInitializer>;
+
+        LocalAssemblerBuilder local_asm_builder(
+            initializer, *this->_local_to_global_index_map);
+
+        DBUG("Calling local assembler builder for all mesh elements.");
+        this->_global_setup.transform(
+                local_asm_builder,
+                this->_mesh.getElements(),
+                _local_assemblers,
+                this->_integration_order,
+                _thermal_conductivity);
+    }
+
+    void createLocalAssemblers() override
+    {
+        if (this->_mesh.getDimension()==1)
+            createLocalAssemblers<1>();
+        else if (this->_mesh.getDimension()==2)
+            createLocalAssemblers<2>();
+        else if (this->_mesh.getDimension()==3)
+            createLocalAssemblers<3>();
+        else
+            assert(false);
+    }
+
+    //! \name ODESystem interface
+    //! @{
+
+    bool isLinear() const override
+    {
+        return true;
+    }
+
+    //! @}
+
+private:
+    Parameter<double, MeshLib::Element const&> const& _thermal_conductivity;
+
+    using LocalAssembler = Freezing::LocalAssemblerDataInterface<
+        typename GlobalSetup::MatrixType, typename GlobalSetup::VectorType>;
+
+
+    void assembleConcreteProcess(const double t, GlobalVector const& x,
+                                 GlobalMatrix& M, GlobalMatrix& K, GlobalVector& b) override
+    {
+        // TODO It looks like, with little work this entire method can be moved to the Process class.
+
+        DBUG("Assemble FreezingProcess.");
+
+        // Call global assembler for each local assembly item.
+        this->_global_setup.executeDereferenced(
+                    *this->_global_assembler, _local_assemblers, t, x, M, K, b);
+    }
+
+
+    std::vector<std::unique_ptr<LocalAssembler>> _local_assemblers;
+};
+
+template <typename GlobalSetup>
+std::unique_ptr<FreezingProcess<GlobalSetup>>
+createFreezingProcess(
+    MeshLib::Mesh& mesh,
+    typename Process<GlobalSetup>::NonlinearSolver& nonlinear_solver,
+    std::unique_ptr<typename Process<GlobalSetup>::TimeDiscretization>&& time_discretization,
+    std::vector<ProcessVariable> const& variables,
+    std::vector<std::unique_ptr<ParameterBase>> const& parameters,
+    BaseLib::ConfigTree const& config)
+{
+    config.checkConfParam("type", "FREEZING");
+
+    DBUG("Create FreezingProcess.");
+
+    // Process variable.
+    ProcessVariable& process_variable =
+        findProcessVariable(config, "process_variable", variables);
+    DBUG("Associate Temperature with process variable \'%s\'.",
+         process_variable.getName().c_str());
+
+    // Thermal conductivity parameter.
+    auto& thermal_conductivity =
+        findParameter<double, MeshLib::Element const&>(
+            config, "thermal_conductivity", parameters);
+
+    DBUG("Use \'%s\' as thermal conductivity parameter.",
+         thermal_conductivity.name.c_str());
+
+    return std::unique_ptr<FreezingProcess<GlobalSetup>>{
+        new FreezingProcess<GlobalSetup>{
+            mesh, nonlinear_solver,std::move(time_discretization),
+            process_variable,
+            thermal_conductivity
+    }};
+}
+}   // namespace ProcessLib
+
+#endif  // PROCESS_LIB_FREEZINGPROCESS_H_
